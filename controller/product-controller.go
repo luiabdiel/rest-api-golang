8856274ae2f269fdec25dfcbd60cca2494d2da76
luiabdiel <luiggiabdieldev@gmail.com>
2026-02-20 11:43:46 -0300

    feat: create product

--- controller/product-controller.go
+++ controller/product-controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"rest-api-golang/model"
 	"rest-api-golang/usecase"
 
 	"github.com/gin-gonic/gin"
@@ -17,11 +18,29 @@ func NewProductController(usecase usecase.ProductUsecase) productController {
 	}
 }
 
-func (p *productController) GetProducts(ctx *gin.Context) {
-	products, err := p.productUsecase.GetProducts()
+func (pc *productController) GetProducts(ctx *gin.Context) {
+	products, err := pc.productUsecase.GetProducts()
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, err)
 	}
 
 	ctx.JSON(http.StatusOK, products)
+}
+
+func (pc *productController) CreateProduct(ctx *gin.Context) {
+	var product model.Product
+
+	err := ctx.BindJSON(&product)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedProduct, err := pc.productUsecase.CreateProduct(product)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedProduct)
 }
